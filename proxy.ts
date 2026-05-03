f67ea5f3c9983c7d95ca73231e368e16a0493d11
ASM Shad <asmshad@gmail.com>
2026-05-03 15:11:48 +0600

    Job board and card all created

--- proxy.ts
+++ proxy.ts
@@ -1,20 +1,15 @@
 import { NextRequest, NextResponse } from "next/server";
 import { getSession } from "./lib/auth/auth";
 
+export default async function proxy(request: NextRequest) {
+  const session = await getSession();
 
-export default async function proxy(request: NextRequest){
-    const session = await getSession()
-    const isDashboard = request.nextUrl.pathname.startsWith("/dashboard")
-    if(isDashboard && !session?.user){
-        return NextResponse.redirect(new URL("/sign-in", request.url))
-    }
+  const isSignInPage = request.nextUrl.pathname.startsWith("/sign-in");
+  const isSignUpPage = request.nextUrl.pathname.startsWith("/sign-up");
 
-    const isSignIn = request.nextUrl.pathname.startsWith("/sign-in");
-    const isSignUp = request.nextUrl.pathname.startsWith("/sign-up");
+  if ((isSignInPage || isSignUpPage) && session?.user) {
+    return NextResponse.redirect(new URL("/dashboard", request.url));
+  }
 
-    if((isSignIn || isSignUp) && session?.user){
-        return NextResponse.redirect(new URL("/dashboard", request.url))
-    }
-
-    return NextResponse.next()
+  return NextResponse.next();
 }
